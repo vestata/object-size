10b129bca3b0ba8dd015ddd689b507f26a2faae1
vestata <tony268596@gmail.com>
2024-08-05 16:59:58 +0800

    Fix the best fit method for `fit_boxes()`
    This version of `fit_boxes()` works better, and fix some bug from the
    older version of `fit_boxes()`.
    
    Also set `environment` in `getUserMedia` to force using rear camera.

--- static/main.js
+++ static/main.js
@@ -128,28 +128,27 @@ function fit_boxes(length, width, height) {
         large: { width: 69, height: 47, depth: 47, volume: 69 * 47 * 47 }   
     };
 
-    const box_order = ['large', 'medium', 'small'];
-    let remaining_volume = length * width * height;
+    small_v = 47 * 33 * 30;
+    medium_v = 48 * 45 * 42;
+    large_v = 69 * 47 * 47;
+
+    let volume = length * width * height;
     let box_count = { small: 0, medium: 0, large: 0 };
 
-    // function can_fit(box, l, w, h) {
-    //     return (l <= box.width && w <= box.height && h <= box.depth) || 
-    //            (l <= box.width && w <= box.depth && h <= box.height) || 
-    //            (l <= box.height && w <= box.width && h <= box.depth) || 
-    //            (l <= box.height && w <= box.depth && h <= box.width) || 
-    //            (l <= box.depth && w <= box.width && h <= box.height) || 
-    //            (l <= box.depth && w <= box.height && h <= box.width);
-    // }
-
-    for (let box_size of box_order) {
-        let box_volume = box_config[box_size].volume;
-        let box = box_config[box_size];
-
-        // while (remaining_volume >= box_volume && can_fit(box, length, width, height)) {
-        while (remaining_volume >= box_volume) {
-            remaining_volume -= box_volume;
-            box_count[box_size]++;
-        }
+    while (volume >= large_v) {
+        volume -= large_v;
+        box_count.large++;
+    }
+    while (volume < large_v && volume >= medium_v){
+        volume -= medium_v;
+        box_count.medium++;
+    }
+    while (volume < medium_v && volume >= small_v){
+        volume -= small_v;
+        box_count.small++;
+    }
+    if(volume >=0){
+        box_count.small++;
     }
 
     return box_count;
